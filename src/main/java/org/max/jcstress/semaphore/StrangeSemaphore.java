bddab2c9c39be3a3a63c5bad397fbef7c21b44fc
Maksym Stepanenko <maksym.stepanenko@opower.com>
2022-07-28 11:58:26 +0300

    Cleanup.

--- src/main/java/org/max/jcstress/semaphore/StrangeSemaphore.java
+++ src/main/java/org/max/jcstress/semaphore/StrangeSemaphore.java
@@ -1,10 +1,6 @@
 package org.max.jcstress.semaphore;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public final class StrangeSemaphore {
 
